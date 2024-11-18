6557f80f0a1a3cf050b615217758f49d26a48fc2
Honoré Niyomukiza <hniyomen@gmail.com>
2024-11-18 18:22:32 +0200

    Create power-menu.sh

---
+++ i3/scripts/power-menu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Show power options using rofi
+chosen=$(echo -e "Shutdown\nReboot\nLog Out\nSuspend\nHibernate" | rofi -dmenu -i -p "Power Menu")
+
+case $chosen in
+    Shutdown)
+        systemctl poweroff
+        ;;
+    Reboot)
+        systemctl reboot
+        ;;
+    Log\ Out)
+        i3-msg exit
+        ;;
+    Suspend)
+        systemctl suspend
+        ;;
+    Hibernate)
+        systemctl hibernate
+        ;;
+    *)
+        exit 0
+        ;;
+esac
